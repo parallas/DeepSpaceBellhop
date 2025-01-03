41ebdd1c3765fe99d68551da140b61635803b1e8
bscit <54488066+tmaster-terrarian@users.noreply.github.com>
2025-01-02 22:47:30 -0800

    fix build scripts 2

--- build.bat
+++ build.bat
@@ -4,8 +4,10 @@ xcopy /y /s ".\SteamAssets\installscripts\dotnet-installscript-win.bat" ".\Eleva
 
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\linux-x64"
 dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+rd /s/q ".\ElevatorGame\bin\Release\net8.0\linux-x64\publish"
 
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\osx-x64"
 dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+rd /s/q ".\ElevatorGame\bin\Release\net8.0\osx-x64\publish"
 
 pause
--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ cp "./SteamAssets/installscripts/dotnet-installscript-win.bat" "./ElevatorGame/b
 
 rm -rf "./ElevatorGame/bin/Release/net8.0/linux-x64"
 dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+rm -rf "./ElevatorGame/bin/Release/net8.0/linux-x64/publish"
 
 rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64"
 dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64/publish"
