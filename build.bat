e4689938d62b434e2bbde1861a9bd1b956a540b5
bscit <54488066+tmaster-terrarian@users.noreply.github.com>
2025-01-02 23:41:30 -0800

    build scripts AGAIN AUUGH

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\win-x64"
-dotnet publish ElevatorGame -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained false
+dotnet publish ElevatorGame -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained false
 xcopy /y /s ".\SteamAssets\installscripts\dotnet-installscript-win.bat" ".\ElevatorGame\bin\Release\net8.0\win-x64\publish"
 
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\linux-x64"
-dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained true
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\linux-x64\publish"
 
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\osx-x64"
-dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained true
 rd /s/q ".\ElevatorGame\bin\Release\net8.0\osx-x64\publish"
 
 pause
--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-rm -rf "./ElevatorGame/bin/Release/net8.0/win-x64"
-dotnet publish ElevatorGame -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained false
-cp "./SteamAssets/installscripts/dotnet-installscript-win.bat" "./ElevatorGame/bin/Release/net8.0/win-x64/publish/"
+rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64"
+dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained true
+rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64/publish"
 
 rm -rf "./ElevatorGame/bin/Release/net8.0/linux-x64"
-dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
+dotnet publish ElevatorGame -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained true
 rm -rf "./ElevatorGame/bin/Release/net8.0/linux-x64/publish"
 
-rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64"
-dotnet publish ElevatorGame -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishAot=true --self-contained true
-rm -rf "./ElevatorGame/bin/Release/net8.0/osx-x64/publish"
+rm -rf "./ElevatorGame/bin/Release/net8.0/win-x64"
+dotnet publish ElevatorGame -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained false
+cp "./SteamAssets/installscripts/dotnet-installscript-win.bat" "./ElevatorGame/bin/Release/net8.0/win-x64/publish/"
+
