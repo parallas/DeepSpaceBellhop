6672ed9cc9cbd3766e7e0dc3e7e658a3da87b2cf
bscit <bscitdv@gmail.com>
2025-03-22 18:17:21 -0700

    add console logging to file

--- ElevatorGame/Program.cs
+++ ElevatorGame/Program.cs
@@ -1,4 +1,33 @@
-﻿var steam = !args.Contains("--no-steam");
+﻿using System.Text;
+
+var steam = !args.Contains("--no-steam");
 
 using var game = new ElevatorGame.MainGame(steam);
+
+FileStream logFile = null;
+StreamWriter logWriter = null;
+if(!Console.IsOutputRedirected)
+{
+    string logPath = Path.Combine(ElevatorGame.FileLocations.ProgramPath, "latest.log");
+    if(File.Exists(logPath + ".old"))
+        File.Delete(logPath + ".old");
+    if(File.Exists(logPath))
+        File.Copy(logPath, logPath + ".old");
+
+    logFile = File.Open(logPath, FileMode.Create, FileAccess.Write);
+    logWriter = new StreamWriter(logFile, Encoding.UTF8)
+    {
+        AutoFlush = true,
+        NewLine = "\n",
+    };
+
+    Console.SetOut(logWriter);
+
+    if(!Console.IsErrorRedirected)
+        Console.SetError(logWriter);
+}
+
 game.Run();
+
+logWriter?.Flush();
+logWriter?.Close();
